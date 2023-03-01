686fdab18c6c3900389b6e363606b62635807007
Nnaebiyite <bibiobuoha@gmail.com>
2023-03-01 13:31:30 -0800

    task 0 and 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,21 +6,20 @@
  * @dest: input value
  * @src: input value
  * @n: input value
- *
  * Return: dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
+int i;
+int j;
 	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
+while (dest[i] != '\0')
+{
+	i++;
+}
 	j = 0;
-	while (j < n && src[j] != '\0')
+while (j < n && src[j] != '\0')
 {
 	dest[i] = src[j];
 	i++;
